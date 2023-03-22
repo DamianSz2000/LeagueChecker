695cd891f176a7e4eaecc0943e888e9a89b9e3ec
DamianSz2000 <92025938+DamianSz2000@users.noreply.github.com>
2023-03-22 12:27:13 +0100

    MainView class refactor
    Made it reflect more of a class rather than pile of code.

--- src/View/MainView.java
+++ src/View/MainView.java
@@ -1,48 +1,62 @@
 package view;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
 public class MainView {
-    public static void createAndShowGUI() {
-        JFrame frame = new JFrame("LeagueChecker");
+    private JFrame frame;
+    private JMenuBar menuBar;
+    private JMenu menu;
+    private JMenuItem menuItem1, menuItem2;
+    private JTable table;
+    private DefaultTableModel model;
+    private JScrollPane scrollPane;
+
+    public void createAndShowGUI() {
+        createFrame();
+        createMenuBar();
+        createTable();
+        customizeColors();
+        frame.setVisible(true);
+    }
+
+    private void createFrame() {
+        frame = new JFrame("LeagueChecker");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1280, 720);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
-        JMenuBar menuBar = new JMenuBar();
-        JMenu menu = new JMenu("File");
-        menuBar.add(menu);
-        JMenuItem menuItem = new JMenuItem("Load combos from file");
-        JMenuItem menuItem2 = new JMenuItem("Load accounts from file");
-        menu.add(menuItem);
+    }
+
+    private void createMenuBar() {
+        menuBar = new JMenuBar();
+        menu = new JMenu("File");
+        menuItem1 = new JMenuItem("Load combos from file");
+        menuItem2 = new JMenuItem("Load accounts from file");
+        menu.add(menuItem1);
         menu.add(menuItem2);
+        menuBar.add(menu);
         frame.setJMenuBar(menuBar);
+    }
+
+    private void createTable() {
         String[] columnNames = {"Region", "Username", "Password", "RP", "BE", "Level", "Champions", "Skins", "Details"};
-        JTable table = new JTable(0, columnNames.length);
+        table = new JTable(0, columnNames.length);
         table.setPreferredScrollableViewportSize(new Dimension(1280, 720));
         table.setFillsViewportHeight(true);
         table.getTableHeader().setReorderingAllowed(false);
-        DefaultTableModel model = (DefaultTableModel) table.getModel();
+        model = (DefaultTableModel) table.getModel();
         model.setColumnIdentifiers(columnNames);
-        JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane = new JScrollPane(table);
         frame.add(scrollPane);
+    }
 
-
+    private void customizeColors() {
         menuBar.setBackground(new Color(141, 153, 174));
         menuBar.setForeground(new Color(255, 255, 255));
         table.setBackground(new Color(43, 45, 66));
         table.getTableHeader().setBackground(new Color(43, 45, 66));
         table.getTableHeader().setForeground(new Color(255, 255, 255));
-
-
-
-
-
-
-
-
-
-        frame.setVisible(true);
     }
 }
