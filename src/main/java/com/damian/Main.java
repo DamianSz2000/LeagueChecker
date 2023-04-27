dfa82c0ae3d8870de916e41a9db405cca1361d4c
DamianSz2000 <92025938+DamianSz2000@users.noreply.github.com>
2023-04-27 07:11:20 +0200

    Basic structure of account info class

--- src/main/java/com/damian/Main.java
+++ src/main/java/com/damian/Main.java
@@ -2,10 +2,17 @@ package com.damian;
 
 import com.damian.view.MainView;
 import com.damian.controller.VirtualClientInstance;
+import com.hawolt.logger.Logger;
+import com.hawolt.virtual.leagueclient.authentication.Session;
+
 public class Main {
     public static void main(String[] args) {
+        Logger logger = new Logger();
         MainView view = new MainView();
         view.createAndShowGUI();
         VirtualClientInstance client = new VirtualClientInstance("kretopeak2", "720912piotr");
+        Session session = client.virtualLeagueClient.getSession();
+
+
     }
 }
--- src/main/java/com/damian/controller/AccountInfo.java
+++ src/main/java/com/damian/controller/AccountInfo.java
@@ -1,4 +1,29 @@
 package com.damian.controller;
 
+import com.hawolt.generic.data.Platform;
+import com.hawolt.virtual.leagueclient.authentication.Session;
+
 public class AccountInfo {
+    public String username;
+    public String password;
+    public Session session;
+    public String sessionToken;
+    public VirtualClientInstance virtualClientInstance;
+    public Platform region;
+    AccountInfo(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.virtualClientInstance = new VirtualClientInstance(username, password);
+        this.session = this.virtualClientInstance.virtualLeagueClient.getSession();
+        this.setSessionToken();
+        this.setRegion();
+    }
+
+    public void setSessionToken() {
+        this.sessionToken = this.virtualClientInstance.virtualLeagueClient.getSession().get("session_token");
+    }
+
+    public void setRegion() {
+        this.region = this.session.platform;
+    }
 }
